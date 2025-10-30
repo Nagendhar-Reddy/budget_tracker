77fc13235ceb429e7fc419588007d96c412307e2
Nagendhar Reddy <nagendhar.reddy@sciverse.co.in>
2025-10-30 15:32:48 +0530

    Add Vercel URL to CORS

--- budget_backend/budget_backend/settings.py
+++ budget_backend/budget_backend/settings.py
@@ -164,7 +164,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://budget-tracker-ewo5.onrender.com"
+    "https://budget-tracker-ewo5.onrender.com",
+    "https://budget-tracker-seven-teal.vercel.app/"
 ]
 
 
