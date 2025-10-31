9aeaf3d9368c3c91b9ac9e1414e36f6ecf45499e
Nagendhar Reddy <nagendhar.reddy@sciverse.co.in>
2025-10-31 09:09:16 +0530

    Configure PostgreSQL for production, SQLite for local

--- budget_backend/budget_backend/settings.py
+++ budget_backend/budget_backend/settings.py
@@ -84,13 +84,23 @@ WSGI_APPLICATION = 'budget_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
-}
+# Use PostgreSQL on Render, SQLite locally
+if os.environ.get('DATABASE_URL'):
+    # Production: PostgreSQL on Render
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    # Local Development: SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
